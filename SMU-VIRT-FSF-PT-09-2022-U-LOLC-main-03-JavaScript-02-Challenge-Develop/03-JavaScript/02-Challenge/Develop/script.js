a878fb59819bb532a355f69e235cb35feb7a8b76
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-04-08 08:31:41 -0500

    var charachteristics

--- SMU-VIRT-FSF-PT-09-2022-U-LOLC-main-03-JavaScript-02-Challenge-Develop/03-JavaScript/02-Challenge/Develop/script.js
+++ SMU-VIRT-FSF-PT-09-2022-U-LOLC-main-03-JavaScript-02-Challenge-Develop/03-JavaScript/02-Challenge/Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var 
-
+var specialChar =['!', "@", '#', '$', '%', '^' , '&' , '*', '(' , ')', '-', '_', '+', '=']
+var capitalLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'H', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'V', 'U', 'W', 'X', 'Y' , 'Z']
+var lowercaseLetters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm ','n','o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
