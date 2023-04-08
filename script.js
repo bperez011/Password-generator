182241341b4ff0c2ecde3efe59b83302f472ece7
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-04-08 09:08:47 -0500

    4 loop for numbers and finall commit

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ var numbers = ['1' , '2', '3', '4' , '5', '6', '7', '8', '9', '0']
 
 //choices of how long and what it will contain
 function passwordOptions(){
-  choices = []
- passwordLength = window.prompt("How long would you like your password to be (between 8-128)?");
+  choices = [] 
+  passwordLength = window.prompt("How long would you like your password to be (between 8-128)?")
   var includeUpper = window.confirm("Would you like your password to have upper cased letters?")
-  if (includeUpper) {
+  if(includeUpper) {
     choices= choices.concat(capitalLetters)
   }
   var includeLower = window.confirm ("Would you like your password to have lower cased letters?")
@@ -28,9 +28,21 @@ function passwordOptions(){
 }  
 
 
+//function to generate password
+function generatePassword (){
+  passwordOptions()
+  var passArray = []
 
+  for (var i = 0; i <passwordLength; i++){
+    var randomIndex = Math.floor(Math.random()* choices.length);
+    password = password + (choices[randomIndex]);
+  }
+  return password;
 
+}
 
+// var to generateBtn 
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
@@ -40,6 +52,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
